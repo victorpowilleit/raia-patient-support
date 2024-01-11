9f06b71a0c91e69cc9ee923cea15576077eec009
Victor Powilleit <victor.powilleit@gmail.com>
2024-01-11 01:48:01 -0300

    v1.2.2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,17 @@ import {VitePWA} from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [react()],
-  plugins: [react(), VitePWA({registerType: 'autoUpdate', devOptions: {enabled: true}})],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {enabled: true},
+      manifest:{
+        orientation: "portrait",
+        lang: "pt-br",
+        name: "Raia Customer Support Counter",
+        short_name: "RCS",
+      }
+    })
+  ],
 })
