6a8d996fddedec306dad149aba76a2dd5d69ed9e
Victor Powilleit <victor.powilleit@gmail.com>
2024-03-15 23:01:09 -0300

    Adds Log Feature

--- src/signals/workers.ts
+++
@@ -1,5 +0,0 @@
-import {signal} from "@preact/signals-react";
-
-export const workers = signal<string[]>([])
-
-export const workerIndex = signal<number|null>(null)
--- src/utils/haptic.ts
+++
@@ -1,6 +0,0 @@
-export function feedback(){
-if('vibrate' in navigator) {
-    // A API Navigator.vibrate() está disponível, então podemos acionar o feedback tátil
-    navigator.vibrate(100); // 100ms de vibração
-  }
-}
